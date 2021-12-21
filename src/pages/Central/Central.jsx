478b0060c209f64444a89c3485ecdccb2d3189d5
carlos eduardo damian perez <dapecarlos@gmail.com>
2021-12-21 09:02:54 -0600

    Add Dashboard

---
+++ src/pages/Central/Central.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Table} from '../../components/'
+import './Central.css'
+
+const Central = () => {
+    return (
+        <div className='central'>
+            <Table/>
+        </div>
+    )
+}
+
+export default Central
---
+++ src/pages/Top/Top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { WidgetContent } from '../../components/WidgetContent/WidgetContent'
+import './top.css'
+const Top = () => {
+
+ 
+  return (
+    <div className='top'>
+     <WidgetContent/>
+    </div>
+  )
+}
+
+export default Top
